e15b132faecce0ccd8dc8a506481e07d98cbd6f3
Eyakub Sorkar <eyakubsorkar@gmail.com>
2020-05-04 06:23:00 +0600

    Updates

---
+++ precache-manifest.d27d28fee803c962cc37f89794d9d82e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d75b9e6dbc9dd8bf32fb590214476a1",
+    "url": "/youtube-clone-reactjs/index.html"
+  },
+  {
+    "revision": "69c4063916210d512f7f",
+    "url": "/youtube-clone-reactjs/static/js/2.674dd14f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone-reactjs/static/js/2.674dd14f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4353476e842fe654864",
+    "url": "/youtube-clone-reactjs/static/js/main.630566b1.chunk.js"
+  },
+  {
+    "revision": "c8fa1f3dbeb03a2cf0c9",
+    "url": "/youtube-clone-reactjs/static/js/runtime-main.a777bf01.js"
+  }
+]);
